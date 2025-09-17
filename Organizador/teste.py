93e8eac8c74718c3af5dd2e61d096fc7bd8c7362
DreamerYoungdev <159437738+DreamerYoungdev@users.noreply.github.com>
2025-09-17 14:45:37 -0300

    Add files via upload

---
+++ Organizador/teste.py
@@ -0,0 +1,5 @@
+from document_processor.main import process_document
+
+
+info = process_document(r"C:\Users\Root\Desktop\codigos do vs\Organizador\document_processor\SavedFiles\ALVARA-EFEITO-COOPERATIVA.pdf")
+print(info)
